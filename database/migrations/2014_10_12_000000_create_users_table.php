622568afc8f0dc91aee92515e67577cc50dc7ab1
anti009x <riskisuleman76@gmail.com>
2024-02-26 08:32:07 +0700

    Update Seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',['admin','operator' , 'karyawan' , 'pembeli'])->default('pembeli');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,3 +31,4 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
+
